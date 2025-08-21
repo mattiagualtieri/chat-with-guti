8f186a3310032a074ef72f08d47d566748cb3539
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2025-08-21 17:32:41 +0200

    Added some logs

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -1,12 +1,29 @@
-from fastapi import WebSocket, APIRouter, WebSocketDisconnect
+import logging
+from fastapi import Response, WebSocket, APIRouter, WebSocketDisconnect
 from uuid import uuid1
 from ..service.agent import ChatAgent
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler('debug.log'),
+        logging.StreamHandler()
+    ]
+)
 
 
 router = APIRouter(
     prefix='',
     tags=['chat']
 )
+
+
+@router.get('/health')
+async def health():
+    logging.info('Called /health')
+    return Response()
 
 
 @router.websocket('/chat')
@@ -17,6 +34,7 @@ async def chat(web_socket: WebSocket):
     try:
         while True:
             message = await web_socket.receive_text()
+            logging.info(f'Received message: {message}')
             response = agent.chat(message, session_key)
             await web_socket.send_text(response)
     except WebSocketDisconnect:
